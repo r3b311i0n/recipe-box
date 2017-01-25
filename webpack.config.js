3b0779d1c50fe1cb9948e97b976bf7d4e2a1085a
r3b311i0n <nasashinega@gmail.com>
2017-01-25 15:45:05 +0530

    init

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.tsx",
+    output: {
+        filename: "bundle.js",
+        path: __dirname + "/dist",
+        publicPath: "/dist/"
+    },
+
+    devtool: "source-map",
+
+    resolve: {
+        extensions: ["", ".webpack.js", ".web.js", "ts", ".tsx", ".js", ".jsx", "css"]
+    },
+
+    module: {
+        loaders: [
+            {test: /\.tsx?$/, loader: "awesome-typescript-loader"},
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader?importLoaders=1!postcss-loader")
+            }
+        ],
+
+        preLoaders: [
+            {test: /\.js$/, loader: "source-map-loader"}
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin("styles.css")
+    ],
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    }
+};
